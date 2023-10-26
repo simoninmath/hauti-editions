8809c6d5df744bc6b5dab27e2d8fcce2554a37a7
SIMONIN Mathieu <simonin.math@gmail.com>
2023-10-26 21:53:08 +0200

    [setup]-install-webpack

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,14 +1,30 @@
-<?php // src/Controller/DefaultController.php namespace App\Controller; 
+<?php
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
-use Symfony\Component\HttpFoundation\Response; 
-use Symfony\Component\Routing\Annotation\Route; 
+namespace App\Controller;
 
-class DefaultController extends AbstractController 
-{ 
-    /** * 
-     * @Route("/app/{slug?}", name="app", requirements={"slug"=".+"})") 
-     * */ 
-    public function app(): Response { return $this->render('app.html.twig'); 
-    } 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class MainController extends AbstractController
+{
+
+    /**
+     * @Route("/app/{slug?}", name="app", requirements={"slug"=".+"})")
+     */
+
+    public function app(): Response
+    {
+        return $this->render('app.html.twig');
+    }
+
+    /**
+     * @Route("/api/helloword/{name}", name="api_helloword")")
+     */
+
+    public function apiHelloword(string $name): Response
+    {
+        return new JsonResponse('hello ' . $name);
+    }
 }
