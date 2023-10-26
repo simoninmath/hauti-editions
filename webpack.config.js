82c6fec826aab771986bfe0e1129fd83184e2f34
SIMONIN Mathieu <simonin.math@gmail.com>
2023-10-26 21:56:15 +0200

    [setup]-modify-webpack-json-file

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,115 @@
-const Encore = require('@symfony/webpack-encore');
+const Encore = require("@symfony/webpack-encore");
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
+
 // It's useful when you use tools that rely on webpack.config.js file.
+
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || "dev");
 }
 
 Encore
-    // directory where compiled assets will be stored
-    .setOutputPath('public/build/')
-    // public path used by the web server to access the output path
-    .setPublicPath('/build')
-    // only needed for CDN's or subdirectory deploy
-    //.setManifestKeyPrefix('build/')
 
-    /*
-     * ENTRY CONFIG
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-     */
-    .addEntry('app', './assets/app.js')
+  // directory where compiled assets will be stored
 
-    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    .splitEntryChunks()
+  .setOutputPath("public/build/")
 
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
+  // public path used by the web server to access the output path
 
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+  .setPublicPath("/build")
 
-    // configure Babel
-    // .configureBabel((config) => {
-    //     config.plugins.push('@babel/a-babel-plugin');
-    // })
+  // only needed for CDN's or sub-directory deploy
 
-    // enables and configure @babel/preset-env polyfills
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = '3.23';
-    })
+  //.setManifestKeyPrefix('build/')
 
-    // enables Sass/SCSS support
-    //.enableSassLoader()
+  /*
 
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+* ENTRY CONFIG
 
-    // uncomment if you use React
-    //.enableReactPreset()
+*
 
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+* Each entry will result in one JavaScript file (e.g. app.js)
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
-;
+* and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+
+*/
+
+  .addEntry("app", "./assets/app.js")
+
+  .enableTypeScriptLoader()
+
+  .enableVueLoader(() => {}, { runtimeCompilerBuild: false })
+
+  // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+
+  .enableStimulusBridge("./assets/controllers.json")
+
+  // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+
+  .splitEntryChunks()
+
+  // will require an extra script tag for runtime.js
+
+  // but, you probably want this, unless you're building a single-page app
+
+  .enableSingleRuntimeChunk()
+
+  /*
+
+* FEATURE CONFIG
+
+*
+
+* Enable & configure other features below. For a full
+
+* list of features, see:
+
+* https://symfony.com/doc/current/frontend.html#adding-more-features
+
+*/
+
+  .cleanupOutputBeforeBuild()
+
+  .enableBuildNotifications()
+
+  .enableSourceMaps(!Encore.isProduction())
+
+  // enables hashed filenames (e.g. app.abc123.css)
+
+  .enableVersioning(Encore.isProduction())
+
+  .configureBabel((config) => {
+    config.plugins.push("@babel/plugin-transform-class-properties");
+  })
+
+  // enables @babel/preset-env polyfills
+
+  .configureBabelPresetEnv((config) => {
+    config.useBuiltIns = "usage";
+
+    config.corejs = 3;
+  });
+
+// enables Sass/SCSS support
+
+//.enableSassLoader()
+
+// uncomment if you use TypeScript
+
+//.enableTypeScriptLoader()
+
+// uncomment if you use React
+
+//.enableReactPreset()
+
+// uncomment to get integrity="..." attributes on your script & link tags
+
+// requires WebpackEncoreBundle 1.4 or higher
+
+//.enableIntegrityHashes(Encore.isProduction())
+
+// uncomment if you're having problems with a jQuery plugin
+
+//.autoProvidejQuery()
 
 module.exports = Encore.getWebpackConfig();
