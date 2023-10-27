2109762c4adf2f546018fdba20321e2a0d25688e
SIMONIN Mathieu <simonin.math@gmail.com>
2023-10-27 22:59:39 +0200

    [setup]-modify-appjs-file

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,8 @@
 import "./styles/app.css";
 import { createApp } from "vue";
 import App from "./js/App.vue";
+import { registerVueControllerComponents } from '@symfony/ux-vue';
 
 createApp(App).mount("#vue-app");
+
+registerVueControllerComponents(require.context('./vue/controllers', true, /\.vue$/));
