7c5aed84a734a9049436e722885ad62957e38df2
SIMONIN Mathieu <simonin.math@gmail.com>
2023-10-29 17:14:59 +0100

    [setup]-install-fontawesome

--- assets/app.js
+++ assets/app.js
@@ -4,8 +4,15 @@ import App from "./js/App.vue";
 import { registerVueControllerComponents } from "@symfony/ux-vue";
 import "./styles/app.scss";
 
+//vueJS
 createApp(App).mount("#vue-app");
-
 registerVueControllerComponents(require.context("./vue/controllers", true, /\.vue$/));
 
+// bootstrap
 require("bootstrap");
+
+//fontawesome
+require("@fortawesome/fontawesome-free/css/all.min.css");
+require("@fortawesome/fontawesome-free/js/all.js");
+
+<i class="fa-solid fa-house"></i>;
