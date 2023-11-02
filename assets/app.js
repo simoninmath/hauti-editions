d4bcfc805636a0e452b8ac392ab6d519898253f9
SIMONIN Mathieu <simonin.math@gmail.com>
2023-11-02 17:41:28 +0100

    [setup]-modify-appjs-file

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,7 @@
-import "./styles/app.css";
-import { createApp } from "vue";
-import App from "./js/App.vue";
-import { registerVueControllerComponents } from "@symfony/ux-vue";
-import "./styles/app.scss";
+import "./bootstrap.js";
+import "./styles/global.scss";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-
-//vueJS
-createApp(App).mount("#vue-app");
-registerVueControllerComponents(require.context("./vue/controllers", true, /\.vue$/));
 
 //bootstrap
 require("bootstrap");
@@ -16,5 +9,3 @@ require("bootstrap");
 //fontawesome
 require("@fortawesome/fontawesome-free/css/all.min.css");
 require("@fortawesome/fontawesome-free/js/all.js");
-
-// <i class="fa-solid fa-house"></i>;
